a6e3e03ff3c53d9d9973aae4aa513cddae384bc7
Siva Koneru <skoneru@uplandsoftware.com>
2023-05-09 23:12:27 +0530

    Add code changes

---
+++ MemcacheConnection.js
@@ -0,0 +1,67 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const memcached = require("memcached");
+const KEY = `account1/balance`;
+const DEFAULT_BALANCE = 100;
+const MAX_EXPIRATION = 60 * 60 * 24 * 30;
+const memcachedClient = new memcached(`${process.env.ENDPOINT}:${process.env.PORT}`);
+exports.resetMemcached = async function () {
+    var ret = new Promise((resolve, reject) => {
+        memcachedClient.set(KEY, DEFAULT_BALANCE, MAX_EXPIRATION, (res, error) => {
+            if (error)
+                resolve(res);
+            else
+                reject(DEFAULT_BALANCE);
+        });
+    });
+    return ret;
+};
+exports.chargeRequestMemcached = async function (input) {
+    var remainingBalance = await getBalanceMemcached(KEY);
+    const charges = getCharges(input);
+    const isAuthorized = authorizeRequest(remainingBalance, charges);
+    if (!authorizeRequest(remainingBalance, charges)) {
+        return {
+            remainingBalance,
+            isAuthorized,
+            charges: 0,
+        };
+    }
+    remainingBalance = await chargeMemcached(KEY, charges);
+    return {
+        remainingBalance,
+        charges,
+        isAuthorized,
+    };
+};
+
+function authorizeRequest(remainingBalance, charges) {
+    return remainingBalance >= charges;
+}
+function getCharges(input) {
+    return input.unit;
+}
+async function getBalanceMemcached(key) {
+    return new Promise((resolve, reject) => {
+        memcachedClient.get(key, (err, data) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(Number(data));
+            }
+        });
+    });
+}
+async function chargeMemcached(key, charges) {
+    return new Promise((resolve, reject) => {
+        memcachedClient.decr(key, charges, (err, result) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                return resolve(Number(result));
+            }
+        });
+    });
+}
---
+++ RedisConnection.js
@@ -0,0 +1,97 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const redis = require("redis");
+const util = require("util");
+const KEY = `account1/balance`;
+const DEFAULT_BALANCE = 100;
+const MAX_EXPIRATION = 60 * 60 * 24 * 30;
+exports.chargeRequestRedis = async function (input) {
+    const redisClient = await getRedisClient();
+    await redisClient.watch(KEY);
+    var remainingBalance = await getBalanceRedis(redisClient, KEY);
+    var charges = getCharges(input);
+    const isAuthorized = authorizeRequest(remainingBalance, charges);
+    if (!isAuthorized) {
+        return {
+            remainingBalance,
+            isAuthorized,
+            charges: 0,
+        };
+    }
+    const chargesDecreased = await chargeRedis(redisClient, KEY, charges);
+    remainingBalance -= charges;
+    await disconnectRedis(redisClient);
+    if (chargesDecreased == null) {
+         return {
+            remainingBalance,
+            isAuthorized: false,
+            charges: 0,
+        };
+    }
+    return {
+        remainingBalance,
+        charges,
+        isAuthorized,
+    };
+};
+exports.resetRedis = async function () {
+    const redisClient = await getRedisClient();
+    const ret = new Promise((resolve, reject) => {
+        redisClient.set(KEY, String(DEFAULT_BALANCE), (err, res) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(DEFAULT_BALANCE);
+            }
+        });
+    });
+    await disconnectRedis(redisClient);
+    return ret;
+};
+async function getRedisClient() {
+    return new Promise((resolve, reject) => {
+        try {
+            const client = new redis.RedisClient({
+                host: process.env.ENDPOINT,
+                port: parseInt(process.env.PORT || "6379"),
+            });
+            client.on("ready", () => {
+                console.log('redis client ready');
+                resolve(client);
+            });
+        }
+        catch (error) {
+            reject(error);
+        }
+    });
+}
+async function disconnectRedis(client) {
+    return new Promise((resolve, reject) => {
+        client.quit((error, res) => {
+            if (error) {
+                reject(error);
+            }
+            else if (res == "OK") {
+                console.log('redis client disconnected');
+                resolve(res);
+            }
+            else {
+                reject("unknown error closing redis connection.");
+            }
+        });
+    });
+}
+function authorizeRequest(remainingBalance, charges) {
+    return remainingBalance >= charges;
+}
+function getCharges(input) {
+    return input.unit || 0;
+}
+async function getBalanceRedis(redisClient, key) {
+    const res = await util.promisify(redisClient.get).bind(redisClient).call(redisClient, key);
+    return parseInt(res || 0);
+}
+async function chargeRedis(redisClient, key, charges) {
+    return util.promisify(redisClient.decrby).bind(redisClient).call(redisClient, key, charges);
+}
